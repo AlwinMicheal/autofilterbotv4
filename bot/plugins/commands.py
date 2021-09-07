6d3c8dab2a4418bbcd57b857cf1f9575a2a92da6
AlwinMicheal <90100045+AlwinMicheal@users.noreply.github.com>
2021-09-07 16:19:28 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @movies_hut_chat
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Group', url="https://t.me/movies_hut_chat"
                                 )
                         ]
                     ]
